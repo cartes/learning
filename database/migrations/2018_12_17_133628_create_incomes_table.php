d5f8449849e81ba135342a1dd6e20aeda30836b4
cartes <cristiancartesa@gmail.com>
2018-12-17 10:45:21 -0300

    Incomes

---
+++ database/migrations/2018_12_17_133628_create_incomes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIncomesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incomes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('buildings_id');
+            $table->foreign('buildings_id')->references('id')->on('buildings');
+            $table->unsignedInteger('users_id');
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incomes');
+    }
+}

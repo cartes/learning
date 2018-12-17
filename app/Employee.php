1588873625657f8dff61c0f86879d9594ca82ee9
cartes <cristiancartesa@gmail.com>
2018-12-17 15:46:11 -0300

    Reparada la tabla employee y nueva tabla socials

--- app/Employee.php
+++ app/Employee.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    const AFP = 1;
+    const ISAPRE = 2;
+    const CCAF = 3;
+    const OTHER = 4;
 }
--- database/migrations/2018_12_17_135722_create_employees_table.php
+++ database/migrations/2018_12_17_135722_create_employees_table.php
@@ -13,17 +13,26 @@ class CreateEmployeesTable extends Migration
      */
     public function up()
     {
+        Schema::create('socials', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->enum('type', [\App\Employee::AFP, \App\Employee::ISAPRE, \App\Employee::CCAF, \App\Employee::OTHER]);
+        });
         Schema::create('employees', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email');
             $table->string('address');
             $table->string('phone');
-            $table->integer('prevision');
-            $table->integer('afp');
-            $table->integer('ccaf');
+            $table->unsignedInteger('prevision_id')->nullable();
+            $table->foreign('prevision_id')->references('id')->on('socials');
+            $table->unsignedInteger('afp_id')->nullable();
+            $table->foreign('afp_id')->references('id')->on('socials');
+            $table->unsignedInteger('ccaf_id')->nullable();
+            $table->foreign('ccaf_id')->references('id')->on('socials');
             $table->timestamp('date_in');
             $table->integer('vacations');
+            $table->integer('charges')->nullable();
 
             $table->unsignedInteger('buildings_id');
             $table->foreign('buildings_id')->references('id')->on('buildings');

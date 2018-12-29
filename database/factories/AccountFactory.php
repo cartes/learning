7643df16f9bd91fb5d6a06dd47304fcfafbcd5fd
cristiancartes <Front_242>
2018-12-29 19:10:45 -0300

    29 de diciembre

--- database/factories/AccountFactory.php
+++ database/factories/AccountFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Account::class, function (Faker $faker) {
     $type = $faker->randomElement([\App\Account::VISTA, \App\Account::CORRIENTE, \App\Account::AHORRO, \App\Account::OTHER]);
     return [
-        'buildings_id' => \App\Building::all()->random()->id,
+        'buildings_id' => null,
         'bank' => $faker->word,
         'number' => $faker->bankAccountNumber,
         'type' => $type,
--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -2,8 +2,21 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Employee::class, function (Faker $faker) {
+$factory->define(App\Social::class, function (Faker $faker) {
+    $social = $faker->randomElement([\App\Employee::AFP, \App\Employee::ISAPRE, \App\Employee::CCAF, \App\Employee::OTHER]);
     return [
-        //
+        'name' => $faker->name,
+        'type' => $type,
+    ];
+});
+
+$factory->define(App\Employee::class, function (Faker $faker){
+    return [
+        'name' => $faker->name,
+        'email' => $faker->email,
+        'address' => $faker->address,
+        'phone' => $faker->phoneNumber,
+        'prevision_id' => null,
+        'afp_id' => null,
     ];
 });
